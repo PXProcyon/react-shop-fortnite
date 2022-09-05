ddca39a794e1875839d78990fc8a943f5e5be7dc
Nikita B <procyon.man.2000@mail.ru>
2022-09-05 16:36:34 +0500

    site complite

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import {Header} from './components/Header'
 import {Footer} from './components/Footer'
+import {Shop} from './components/Shop'
 
 function App() {
   return (
     <>
       <Header />
-       
+       <Shop />
       <Footer />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
 function Header() {
-    return  <nav className="#64b5f6 blue lighten-2">
-    <div className="nav-wrapper">
-      <a href="#" className="brand-logo right">Kinosaur</a>
-      <ul id="nav-mobile" className="left hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
+  return (
+    <nav className="#000000 black">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo right">
+          Fortnite Shop
+        </a>
+        <ul id="nav-mobile" className="left hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Sass</a>
+          </li>
+          <li>
+            <a href="badges.html">Components</a>
+          </li>
+          <li>
+            <a href="collapsible.html">JavaScript</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export {Header}
+export { Header };
